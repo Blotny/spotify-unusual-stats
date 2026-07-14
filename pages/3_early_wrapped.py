d24ead28963928c6d06beffc15d96d675e2c10b1
Filip Błotny <blotnyfilip@gmail.com>
2026-07-14 22:34:48 +0200

    feat: add early wrapped page with listening metrics and top 5 artists/tracks

--- pages/3_early_wrapped.py
+++ pages/3_early_wrapped.py
@@ -61,3 +61,71 @@ with col4:
     st.metric("Days of listening", f"{days_of_listening}/{day_of_year}")
 
 
+# ulubieni artysci/piosenki
+
+# top artysci
+top_artists = (
+    df_current_year.groupby("artist_name")
+    .agg(ms_sum=("ms_played", "sum"))
+    .reset_index()
+    .sort_values("ms_sum", ascending=False)
+    .head(5)
+)
+
+# top piosenki
+top_tracks = (
+    df_current_year
+    .groupby(["track_name", "artist_name"])
+    .agg(plays=("ms_played", "count"))
+    .reset_index()
+    .sort_values("plays", ascending=False)
+    .head(5)
+)
+
+# najczesciej sluchani artysci/utwory
+
+top_artist = top_artists.iloc[0]
+top_track = top_tracks.iloc[0]
+
+top_artist_name = top_artist["artist_name"]
+top_artist_minutes = int(top_artist["ms_sum"] / 60_000)
+
+top_track_name = top_track["track_name"]
+top_track_plays = int(top_track["plays"])
+
+metric_col1, metric_col2 = st.columns(2)
+
+with metric_col1:
+    st.metric(
+        label="Your top artist",
+        value=top_artist_name,
+        delta=f"{top_artist_minutes} minutes",
+        delta_color="off"
+    )
+
+with metric_col2:
+    st.metric(
+        label="Your top track",
+        value=top_track_name,
+        delta=f"{top_track['plays']} plays",
+        delta_color="off"
+    )
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Favourite artists")
+    for i, row in enumerate(top_artists.itertuples(), start=1):
+        st.markdown(
+            f"<p><strong>{i}</strong> &nbsp;&nbsp; {row.artist_name}</p>",
+            unsafe_allow_html=True
+        )
+
+with col2:
+    st.subheader("Favourite tracks")
+    for i, row in enumerate(top_tracks.itertuples(), start=1):
+        st.markdown(
+            f"<p><strong>{i}</strong> &nbsp;&nbsp; {row.track_name}</p>",
+            unsafe_allow_html=True
+        )
